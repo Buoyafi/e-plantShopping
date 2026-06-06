8f5274a1da954ab57e92c5c208612eeeb7bd09d4
Buoyafi <buoyafibm@gmail.com>
2026-06-06 04:26:32 -0400

    Initial commit

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -19,10 +19,10 @@ export const CartSlice = createSlice({
         }
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+        state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-        const { name, quantity } = ation.payload; // Destructure the product name and new quantity from the action payload
+        const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
         // Find the item in the cart that matches the given name
         const itemToUpdate = state.items.find(item => item.name === name);
         if (itemToUpdate) {
